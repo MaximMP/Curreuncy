fcc407f6560ab303c96535cb49533e3c389d94a4
MaximMP <zo.bordeini@gmail.com>
2020-02-20 19:39:53 +0200

    style

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function calc() {
-  fetch("item.json")
+const currencyFrom = document.getElementById("curreuncy-one");
+const currencyTo = document.getElementById("curreuncy-two");
+const amountFrom = document.getElementById("amount-one");
+const amountTo = document.getElementById("amount-two");
+const spaw = document.getElementById("swap");
+const rate = document.getElementById("rate");
+
+currencyFrom.addEventListener("change", calculate);
+currencyTo.addEventListener("change", calculate);
+amountFrom.addEventListener("input", calculate);
+amountTo.addEventListener("input", calculate);
+
+function calculate() {
+  const currencyFromVaule = currencyFrom.value;
+  const currencyToVaule = currencyTo.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyFromVaule}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+      const rateVaule = data.rates[currencyToVaule];
+      rate.innerText = `1 ${currencyFromVaule} = ${rateVaule} ${currencyToVaule}`;
+      amountTo.value = (amountFrom.value * rateVaule).toFixed(2);
+    });
 }
-calc();
+swap.addEventListener("click", () => {
+  const temp = currencyFrom.value;
+  currencyFrom.value = currencyTo.value;
+  currencyTo.value = temp;
+  calculate();
+});
